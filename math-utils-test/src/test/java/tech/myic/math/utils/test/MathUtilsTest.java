2a7e41f72392c31af30f208244b807d3d86c09bd
jules <jules.nsenda@gmail.com>
2020-07-27 19:58:27 +0200

    Added new test case

--- math-utils-test/src/test/java/tech/myic/math/utils/test/MathUtilsTest.java
+++ math-utils-test/src/test/java/tech/myic/math/utils/test/MathUtilsTest.java
@@ -1,6 +1,6 @@
 package tech.myic.math.utils.test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class MathUtilsTest {
@@ -9,13 +9,19 @@ public class MathUtilsTest {
     }
 
     @Test
-    public void testAdd() {
+    public void testAdd_success() {
         System.out.println("add");
         int a = 19;
         int b = 10;
         int expResult = 29;
         int result = MathUtils.add(a, b);
         assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testAdd_fail() {
+        int result = MathUtils.add(1290, 0);
+        assertNotEquals(1, result);
     }
 
 }
