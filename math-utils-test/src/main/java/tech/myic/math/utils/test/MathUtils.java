78947ddf2c871f437ac08c003ecdd8cee5599b5c
jules <jules.nsenda@gmail.com>
2020-07-27 18:40:13 +0200

    Remove comment

--- math-utils-test/src/main/java/tech/myic/math/utils/test/MathUtils.java
+++ math-utils-test/src/main/java/tech/myic/math/utils/test/MathUtils.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tech.myic.math.utils.test;
 
-/**
- *
- * @author jules
- */
 public class MathUtils {
 
     public static int add(int a, int b) {
--- math-utils-test/src/test/java/tech/myic/math/utils/test/MathUtilsTest.java
+++ math-utils-test/src/test/java/tech/myic/math/utils/test/MathUtilsTest.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tech.myic.math.utils.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-/**
- *
- * @author jules
- */
 public class MathUtilsTest {
 
     public MathUtilsTest() {
